d4aebb48e17506122187111977462309b7432bb4
andgerald <geraldinebecerra93@gmail.com>
2020-05-06 00:15:36 -0400

    arreglo pagina acceder

--- src/lib/registro.js
+++ src/lib/registro.js
@@ -1,34 +1,55 @@
-import {registry, datoUser} from './index.js'
-export function registryView (){
-    let registro= document.getElementById('root');
-    registro.innerHTML='';
-    const desplegaRegistry=`
-    <div class='containerRegistry'>
-        <div class 'registry'>
-            <p class='letter'> Nombre <input type='text' placeholder='ingresa Nombre' class='input' id='nombre'></p>
-            <p class='letter'> Apellido <input type='text' placeholder='ingresa Apellido' class='input' id='apellido'></p>
-            <p class='letter'> UserName <input type='text' placeholder='nombre usuario' class='input' id='nick2'></p>
-            <p class='letter'> Email <input type='text' placeholder='ingresa email' class='input' id='email2'></p>
-            <p class='letter'> Contraseña <input type='password' placeholder='ingresa contrasena' class='input' id='password2'></p>
-        </div>
+import { registry, datoUser } from './index.js'
+export function registryView() {
+    let registro = document.getElementById('root');
+    registro.innerHTML = '';
+    const desplegaRegistry = `
+    <div class="logo-registry">
+        <img src='img/logo-weservice.png'>
     </div>
+    <form class='form-registry'>
+        <div form-field'>
+            <label class='letter'> Nombre </label>
+            <input type='text' id='nombre' class='input' placeholder='ingresa Nombre'  >
+        </div>
+        <div form-field'>
+            <label class='letter'> Apellido </label>
+            <input type='text' id='apellido' class='input' placeholder='ingresa Apellido'  >
+        </div>
+        <div form-field'>
+            <label class='letter'> UserName </label>
+            <input type='text' id='nick2' class='input'  placeholder='nombre usuario' >
+        </div>
+        <div form-field'>
+            <label class='letter'> Email </label>
+            <input type='text' id='email2' class='input' placeholder='ingresa email' >
+        </div>
+        <div form-field'>
+            <label class='letter'> Contraseña </label>
+            <input type='password' id='password2' class='input' placeholder='ingresa contrasena' >
+        </div>
+        </form>
+
+        
+
     <button id='enterTheWall'class='btnStart'>Crear Cuenta</button>
     `
-    registro.innerHTML= desplegaRegistry;
+    registro.innerHTML = desplegaRegistry;
     const btnEnterWall = document.getElementById('enterTheWall');
-    btnEnterWall.addEventListener('click', ()=> {    
+    btnEnterWall.addEventListener('click', () => {
         const name = document.getElementById('nombre').value;
-        const lastName =document.getElementById('apellido').value;
-        const nusuario= document.getElementById('nick2').value;
-        const correoRegistry= document.getElementById('email2').value;
+        const lastName = document.getElementById('apellido').value;
+        const nusuario = document.getElementById('nick2').value;
+        const correoRegistry = document.getElementById('email2').value;
         const claveRegistry = document.getElementById('password2').value;
         registry(correoRegistry, claveRegistry);
-        datoUser(name,lastName,nusuario,correoRegistry,claveRegistry)
+        datoUser(name, lastName, nusuario, correoRegistry, claveRegistry)
         console.log(name)
         console.log(lastName)
         console.log(nusuario)
         console.log(correoRegistry)
         console.log(claveRegistry)
-        window.location.hash='start'
+        window.location.hash = 'start'
     });
 }
+
+
--- src/lib/views.js
+++ src/lib/views.js
@@ -1,38 +1,46 @@
-import {entrar, googleRegistration} from './index.js';
-import { registryView } from './registro.js';
-export function loginView(){
-    let inicio = document.getElementById('root');
-    inicio.innerHTML=''
-    const desplegar= 
-    `<img src='img/time-service1.jpg' class='topImg'>
-    <img src='img/logo-weservice.png' class='logo'>
-    <div class='containerStart'>
-        <div class='start'>
-           <p class='letter'> Email<input type='text' placeholder='ingresa email' class='input' id='email'></p>
-           <p class='letter'>Contraseña <input type='password' placeholder='igresa contraseña' class='input' id='password'></p>
+    import {entrar, googleRegistration} from './index.js';
+    import { registryView } from './registro.js';
+    export function loginView(){
+        let inicio = document.getElementById('root');
+        inicio.innerHTML=''
+        const desplegar= 
+        `<img src='img/time-service1.jpg' class='topImg'>
+        <div class="container-logo">
+            <img src='img/logo-weservice.png' class='logo'>
         </div>
-    </div>
-    <button id='enter' class='btnStart'>Ingresar</button>
-    <button id='createAccount'class='btnStart'>Crear Cuenta</button>
-    <button id='createGoogle'class='btnStart'>Ingresa con Google</button>`
-    
-    inicio.innerHTML=desplegar;
-    const btnEnter = document.getElementById('enter');
-    btnEnter.addEventListener('click', ()=> {
-        const correo=document.getElementById('email').value;
-        const clave= document.getElementById('password').value;
-        entrar(correo, clave);    
-        console.log(clave)
-        console.log(correo)
-    });    
-    const btnCreateAccount=document.getElementById('createAccount');
-    btnCreateAccount.addEventListener('click',()=> {
-        window.location.hash='registry';
-        registryView();
-    })   
-    const btnGoogle = document.getElementById('createGoogle');
-    btnGoogle.addEventListener('click',()=>{
-     console.log('yeeess')
-     googleRegistration();
-    })
-}
+        <form class='form'>
+            <div class='form-field'>
+                <label class='letter'> Email</label>
+                <input type='text' placeholder='ingresa email' class='input' id='email'>
+            </div>  
+            <div class='form-field'>
+                <label class='letter'>Contraseña </label>
+                <input type='password' placeholder='ingresa contraseña' class='input' id='password'>
+            </div>
+        </form>
+        <div class="buttons">
+            <button id='enter' class='btnStart'>Ingresar</button>
+            <button id='createAccount'class='btnStart'>Crear Cuenta</button>
+            <button id='createGoogle'class='btnStart'>Ingresa con Google</button>
+        </div>`
+        
+        inicio.innerHTML=desplegar;
+        const btnEnter = document.getElementById('enter');
+        btnEnter.addEventListener('click', ()=> {
+            const correo=document.getElementById('email').value;
+            const clave= document.getElementById('password').value;
+            entrar(correo, clave);    
+            console.log(clave)
+            console.log(correo)
+        });    
+        const btnCreateAccount=document.getElementById('createAccount');
+        btnCreateAccount.addEventListener('click',()=> {
+            window.location.hash='registry';
+            registryView();
+        })   
+        const btnGoogle = document.getElementById('createGoogle');
+        btnGoogle.addEventListener('click',()=>{
+        console.log('yeeess')
+        googleRegistration();
+        })
+    }
